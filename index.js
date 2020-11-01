0dfa26ab913eceace4c79c9a0ea3b5e46b6125d8
farfar336 <farrukh.naghiyev@ucalgary.ca>
2020-10-31 20:08:16 -0600

    Added style.css file

--- index.js
+++ index.js
@@ -1,40 +1,33 @@
+const page_open_message = 'A user connected'
+const page_closed_message = 'A user disconnected'
+
 var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html'); //What html file will be sent to the webpage to display
 });
 
-io.on('connection', (socket) => {
-  console.log('a user connected');
+http.listen(3000, () => { //Specifies what port number to for hosting
+  console.log('listening on *:3000');
 });
 
-io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('disconnect', () => {
-      console.log('user disconnected');
+io.on('connection', (connection_socket) => {  //Executed when a user opens the page
+      console.log(page_open_message); 
+    connection_socket.on('disconnect', () => { //Executed when a user closes the page
+      console.log(page_closed_message);
     });
-  });
-
-io.on('connection', (socket) => {
-socket.on('chat message', (msg) => {
-    console.log('message: ' + msg);
 });
+
+io.on('connection', (socket) => { //Unknown
+    socket.broadcast.emit('hi');
+});
+
+io.on('connection', (message_socket) => { //Executed when a user presses 'send'
+  message_socket.on('chat message', (messsage) => { 
+    io.emit('chat message', messsage); //Displays message to everyone
+  });
 });
 
 io.emit('some event', { someProperty: 'some value', otherProperty: 'other value' }); // This will emit the event to all connected sockets
-
-io.on('connection', (socket) => {
-    socket.broadcast.emit('hi');
-  });
-
-  io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
-    });
-  });
-
-http.listen(3000, () => {
-  console.log('listening on *:3000');
-});
